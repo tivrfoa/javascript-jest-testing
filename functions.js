e0584dec454e177e728901b535120d1058f53e6b
heyjo <heyjo@notvalid.com>
2019-08-11 22:08:50 -0300

    test expect toBeNull

--- functions.js
+++ functions.js
@@ -2,6 +2,7 @@
 
 const functions = {
     add: (a, b) => a + b,
+    isNull: () => null
 }
 
 export default functions;
--- functions.test.js
+++ functions.test.js
@@ -8,3 +8,7 @@ test('Adds 2 + 2 to NOT equal 5', () => {
     expect(functions.add(2, 2)).not.toBe(5);
 });
 
+test('Should be null', () => {
+    expect(functions.isNull()).toBeNull();
+});
+
