9cce075de4b03e9e633a765658176cc8180b78f8
heyjo <heyjo@notvalid.com>
2019-08-11 22:06:08 -0300

    test expect NOT toBe

--- functions.test.js
+++ functions.test.js
@@ -3,3 +3,8 @@ import functions from './functions';
 test('Adds 2 + 2 to equal 4', () => {
     expect(functions.add(2, 2)).toBe(4);
 });
+
+test('Adds 2 + 2 to NOT equal 5', () => {
+    expect(functions.add(2, 2)).not.toBe(5);
+});
+
